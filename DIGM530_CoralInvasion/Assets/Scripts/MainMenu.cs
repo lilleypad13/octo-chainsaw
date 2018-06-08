997be7cffa335749edd85b960d5aab9c2ace8812
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2018-06-08 14:51:21 -0400

    Updates Based on Grad Playtesting
    Updated parameters, sounds and some colliders

--- DIGM530_CoralInvasion/Assets/Scripts/MainMenu.cs
+++ DIGM530_CoralInvasion/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame ()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
     
     public void QuitGame ()
